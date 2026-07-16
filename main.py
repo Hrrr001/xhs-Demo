0d531e6da8d7623f44f95c8ff314a4c15794818b
16963 <lengge00094@163.com>
2026-07-16 14:18:25 +0800

    feat: 开启Gradio share隧道，启动即获公网链接

--- main.py
+++ main.py
@@ -422,7 +422,7 @@ if __name__ == "__main__":
     app.launch(
         server_name="0.0.0.0",
         server_port=7860,
-        share=False,
+        share=True,
         inbrowser=False,
         css=CUSTOM_CSS,
         theme=gr.themes.Soft(),
